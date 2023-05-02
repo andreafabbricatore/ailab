6ca56190be044590ce9da32a442b7d20d756b669
Toto <antonio.calderoni.ac@gmail.com>
2023-05-02 15:04:05 +0200

    fix_data

--- fix_data.py
+++ fix_data.py
@@ -7,9 +7,7 @@ def name_to_label(name):
     else: raise Exception
 
 
-def add_label_T(df, keep_name=False):
-    df = df.T.reset_index(drop=False)
-    df = df.rename(columns={"index": "name"})
-    df["label"] = df["name"].apply(name_to_label)
-    if not keep_name: df = df.drop("name", axis=1)
+def add_label_T(df):
+    df = df.T
+    df["label"] = (df.index).map(name_to_label)
     return df
