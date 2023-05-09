91ac5383ac39875ad29f98273951fa14d0c0d53a
Alessandro Pranzo <alessandropranzo@MacBook-Pro-di-Alessandro-2.local>
2023-05-09 16:14:36 +0200

    MCF7 smarts first analysis

--- fix_data.py
+++ fix_data.py
@@ -1,13 +1,23 @@
 import pandas as pd
+import numpy as np
 
 
 def name_to_label(name):
-    if "orm" in name: return 1
-    elif "ypo" in name: return 0
-    else: raise Exception
+    if "orm" in name:
+        return 1
+    elif "ypo" in name:
+        return 0
+    else:
+        raise Exception
 
 
 def add_label_T(df):
     df = df.T
     df["label"] = (df.index).map(name_to_label)
     return df
+
+
+def calc_sparsity(matrix):
+    sparsity = 1.0 - (np.count_nonzero(matrix) / float(matrix.size))
+
+    return sparsity
